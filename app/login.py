5b6f1ace9f06effebe20014534012e554cc6f2bc
shreyaasridhar <shreyanju@gmail.com>
2016-11-17 00:39:53 +0530

    Updated Readme

--- app/login.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import Form  
-from wtforms import StringField, BooleanField, PasswordField, TextAreaField, validators
-from wtforms.validators import InputRequired, Length
-
-class loginform(Form):
-    username = StringField(u'User Name:', validators=[InputRequired(), Length(max=30)])
-    passwd = PasswordField(u'Password:', validators=[Length(min=4, max=16)])
